9ed4da9193b3441b87a1aecf745bda3eeb3cd11d
kirk <kekirk@163.com>
2017-07-17 06:31:03 +0000

    remove name

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5  -- Kirk New World!</div>
+                <div class="title">Laravel 5  -- FT World!</div>
             </div>
         </div>
     </body>
